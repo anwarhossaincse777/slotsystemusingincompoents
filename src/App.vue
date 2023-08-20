5c5548a86b9cdadb5724f522ce50539534cbe323
Betabloker <72620275+Betabloker@users.noreply.github.com>
2023-08-20 12:10:01 +0600

    SlotUsingProcess

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+
+import ButtonComponent from "./components/ButtonComponent.vue";
+import {ref} from "vue";
+import SlotComponentsUsingSystem from "./components/SlotComponentsUsingSystem.vue";
+
+const msg = ref("Hello World")
+
+function clickHandlerOne() {
+  msg.value = "Click Button One"
+}
+
+function clickHandlerTwo() {
+  msg.value = "Click Button Number Two"
+}
+
+</script>
+
+<template>
+  <h2 class="text-2xl my-10">Slot using in  props system</h2>
+  <p class="my-5">
+    {{ msg }}
+  </p>
+
+  <ButtonComponent :clickHandler="clickHandlerOne" >
+    <em>Button One</em>
+  </ButtonComponent>
+
+  <br>
+  <br>
+  <br>
+  <br>
+
+<!--  another function passing clickHandler attributes-->
+
+  <ButtonComponent :clickHandler="clickHandlerTwo" >
+    <em>Button Two</em>
+  </ButtonComponent>
+
+  <br>
+  <br>
+  <br>
+  <br>
+
+
+<!--  ..slot component with extends layout name-->
+
+
+  <SlotComponentsUsingSystem>
+    Default Content 123
+
+    <template #header>
+      <h2 class="text-2xl my-10">This is Heading</h2>
+    </template>
+
+    <template v-slot:footer>
+      <h2 class="text-xl my-10">This is footer</h2>
+    </template>
+
+  </SlotComponentsUsingSystem>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SlotComponentsUsingSystem.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <p>
+    <slot name="header"></slot>
+  </p>
+  <p>
+    <slot name="default">This is default content</slot>
+  </p>
+  <p>
+    <slot name="footer"></slot>
+  </p>
+</template>
+
+<style scoped>
+
+</style>
